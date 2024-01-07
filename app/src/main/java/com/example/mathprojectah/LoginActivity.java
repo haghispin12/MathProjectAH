67dcdad06be333c33bbd2b2ff75e0e750e754065
user <hag.hispin12@gmail.com>
2024-01-07 14:10:27 +0200

    version2

--- app/src/main/java/com/example/mathprojectah/LoginActivity.java
+++ app/src/main/java/com/example/mathprojectah/LoginActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+
 
 public class LoginActivity extends AppCompatActivity {
     EditText name;
@@ -19,18 +22,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
 
+
+        ///
+
+    }
+
+
+
+    private void initView() {
         name = findViewById(R.id.name);
-        vm = new ModelView();
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPreferences",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                getIntent().putExtra("name",name.getText().toString());
+                intent.putExtra("name",name.getText().toString());
                 startActivity(intent);
                 finish();
+
+                editor.putString("saved",name.getText().toString());
+                editor.apply();
             }
         });
+        name.setText(sharedPreferences.getString("saved",""));
     }
 }
